56e89fcdda1a5230706e56c36680308c57556d9f
Delon Borges <delon.borges@icloud.com>
2022-07-12 16:38:23 -0300

    feat: create backend app

---
+++ backend/devsuperior-dsmeta/src/main/java/com/delonborges/devsuperiordsmeta/DevsuperiorDsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.delonborges.devsuperiordsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuperiorDsmetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevsuperiorDsmetaApplication.class, args);
+    }
+
+}
