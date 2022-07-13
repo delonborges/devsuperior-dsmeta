4b02380b65f59f3b3b159bd0e5e3345e45d77dec
Delon Barbosa Borges <delon_borges@sicredi.com.br>
2022-07-13 13:32:18 -0300

    feat: create SalesCard component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -5,14 +5,16 @@ import './styles.css'
 
 function Header() {
     return (
-        <div className="dsmeta-logo-container">
-            <img src={logo} alt="DSMeta Project Logo"/>
-            <h1>DSMeta</h1>
-            <a href="https://github.com/delonborges" target="_blank" rel="noreferrer">
-                <img src={github_logo} alt="GitHub Logo"/>
-                <p>/delonborges</p>
-            </a>
-        </div>
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta Project Logo"/>
+                <h1>DSMeta</h1>
+                <a href="https://github.com/delonborges" target="_blank" rel="noreferrer">
+                    <img src={github_logo} alt="GitHub Logo"/>
+                    <p>/delonborges</p>
+                </a>
+            </div>
+        </header>
     )
 }
 
