990d5c4d20a954e2287127aeaae16f00fe7d4f57
Delon Borges <delon.borges@icloud.com>
2022-07-15 18:54:34 -0300

    chore: remove backup folder

---
+++ backend/src/main/java/com/delonborges/dsmeta/entities/Sale.java
@@ -0,0 +1,13 @@
+package com.delonborges.dsmeta.entities;
+
+import java.time.LocalDate;
+
+public class Sale {
+
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+}
