9bdc343ae738d6580ee781399c15241fbc75866e
Delon Borges <delon.borges@icloud.com>
2022-07-15 19:37:14 -0300

    feat: create tb_sales class and configure H2 database

--- backend/src/main/java/com/delonborges/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/delonborges/dsmeta/entities/Sale.java
@@ -1,10 +1,24 @@
 package com.delonborges.dsmeta.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "tb_sales")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Sale {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
+
     private String sellerName;
     private Integer visited;
     private Integer deals;
